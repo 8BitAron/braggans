ca76ebf1c767c021b68fd6eb5a3e7dd29a5532dc
Aron Braggans <aronbraggans@cargillmacbookpro.local>
2018-12-19 10:41:55 -0600

    div wraper

--- src/components/App.js
+++ src/components/App.js
@@ -4,52 +4,52 @@ import styled from 'styled-components'
 import Header from '../components/header/header'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Content>
-          <Purple>Live</Purple>
-          <Black> => </Black>
-          <Blue>Ride</Blue>
-          <Black> => </Black>
-          <Orange>Work</Orange>
-          <Black> => </Black>
-          <Purple>Live</Purple>
-        </Content>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Header />
+                <Content>
+                    <div>
+                        <Purple>Live</Purple>
+                        <Black> => </Black>
+                        <Blue>Ride</Blue>
+                        <Black> => </Black>
+                        <Orange>Work</Orange>
+                    </div>
+                </Content>
+            </div>
+        )
+    }
 }
 
 export default App
 
 const Content = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-  height: calc(100vh - 85px);
-  background-color: #ffffff;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+    height: calc(100vh - 85px);
+    background-color: #ffffff;
 `
 const Purple = styled.span`
-  color: #8a07bd;
-  font-size: 50px;
+    color: #8a07bd;
+    font-size: 50px;
 `
 const Orange = styled.span`
-  color: #f56407;
-  font-size: 50px;
+    color: #f56407;
+    font-size: 50px;
 `
 const Blue = styled.span`
-  color: #080aef;
-  font-size: 50px;
+    color: #080aef;
+    font-size: 50px;
 `
 // const Gray = styled.span`
 //   color: #f3f3f0;
 //   font-size: 50px;
 // `
 const Black = styled.span`
-  color: #222222;
-  font-size: 50px;
+    color: #222222;
+    font-size: 50px;
 `
