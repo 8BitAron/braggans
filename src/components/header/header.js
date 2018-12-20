54db1463e02a98e1799e4e4e753ed908213afa7c
Aron Braggans <aronbraggans@Eleanor-Rigby.lan>
2018-12-19 20:04:30 -0600

    adding font awesome

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default class Header extends Component {
   render() {
     return (
       <Wrapper>
         <Links>
-          <Link href="https://github.com/8bitaron">GitHub</Link>
-          <Link href="https://www.linkedin.com/in/aronbraggans/">Linkedin</Link>
+          <Link href="https://github.com/8bitaron">
+            <FontAwesomeIcon
+              icon={['fab', 'github-square']}
+              style={{ marginRight: 5 }}
+            />
+            GitHub
+          </Link>
+          <Link href="https://www.linkedin.com/in/aronbraggans/">
+            <FontAwesomeIcon
+              icon={['fab', 'linkedin']}
+              style={{ marginRight: 5 }}
+            />
+            Linkedin
+          </Link>
         </Links>
-        <Navbar>Coming Soon...</Navbar>
+        <Navbar>
+          <Purple>Live</Purple>
+          <Black> > </Black>
+          <Blue>Ride</Blue>
+          <Black> > </Black>
+          <Orange>Work</Orange>
+        </Navbar>
       </Wrapper>
     )
   }
@@ -31,9 +50,8 @@ const Link = styled.a`
   padding: 0px 10px 0px 0px;
   font-size: 14px;
   font-weight: 700;
-
   &:hover {
-    color: #ffffff;
+    color: #f56407;
   }
 `
 
@@ -48,4 +66,20 @@ const Navbar = styled.nav`
   font-size: 25px;
   font-weight: 700;
   padding: 0px 0px 0px 20px;
+`
+
+const Purple = styled.span`
+  color: #8a07bd;
+`
+const Orange = styled.span`
+  color: #f56407;
+`
+const Blue = styled.span`
+  color: #080aef;
+`
+// const Gray = styled.span`
+//   color: #f3f3f0;
+// `
+const Black = styled.span`
+  color: #222222;
 `
